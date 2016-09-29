0489298dcd73072ed451aa898a96854ce48b8f05
Alex Epelde <aepelde@gmail.com>
2016-09-29 15:42:34 +0200

    Extending Application class

---
+++ app/src/main/java/io/github/epelde/okremote/OkRemoteApp.java
@@ -0,0 +1,14 @@
+package io.github.epelde.okremote;
+
+import android.app.Application;
+
+/**
+ * Created by epelde on 29/9/16.
+ */
+public class OkRemoteApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
